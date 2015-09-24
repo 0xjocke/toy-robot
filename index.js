97a632be3933b6e6bca41d307a3c18222eba6482
Bachstatter <joachim.bachstatter@gmail.com>
2015-09-25 00:04:11 +0200

    Added description for READ command

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-#!/usr/bin/env babel-node
-
 import makeStore from './src/store';
 import {handleCommand} from './src/parser';
 import 'colors';
@@ -25,14 +23,17 @@ console.log('RIGHT'.bold.underline);
 console.log('- Will rotate the robot 90 degrees to the right');
 console.log('REPORT'.bold.underline);
 console.log('- The robot will say the current position and facing direction');
+console.log('READ textfiles/example.txt'.bold.underline);
+console.log('- You can use any textfile on your computer. The robot will execute all valid commands.');
 
 console.log();
 console.log('----------------------------------------------------------------------------------');
 console.log();
-console.log('You have a table size of 5x5 unit to play');
+console.log('You have a table size of 5x5 units to play with.');
 console.log('Please note that our robot is smart and will not do anything that can hurt him.');
 console.log('He will ignore commands making him fall out of the table.');
-console.log('Also note that he wont listen to any commands if you havn\'t placed him.');
+console.log('Also note that he wont listen to any commands if you haven\'t placed him.');
+console.log('If you try to kill him or using invalid commands he will try to tell you what went wrong.');
 console.log();
 
 function handleLineInput(input){
@@ -49,6 +50,3 @@ readline
 })
 .setPrompt('Robot> ');
 readline.prompt();
-
-
-
